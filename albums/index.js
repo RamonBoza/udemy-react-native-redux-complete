0cc3a62baefe8beb0316215c61e0351ffb380cf3
Ramon Boza <bozanayade@gmail.com>
2018-07-20 15:57:59 +0200

    Section 7 - adding AlbumList component

--- albums/index.js
+++ albums/index.js
@@ -1,12 +1,17 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => {
     return (
-        <Header headerText={'Albums'}/>
+        <View>
+            <Header headerText={'Albums'}/>
+            <AlbumList/>
+        </View>
+
     );
 };
 
