beff8f2e956ed0229dcc3b2b237192f016140352
Ramon.Boza <ramon.boza@barltm5484.midasplayer.local>
2018-07-20 15:35:57 +0200

    Section 6 - adding first component, the header

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,17 @@
-/** @format */
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a component
+const App = () => {
+    return (
+        <Header headerText={'Albums'}/>
+    );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+
+
