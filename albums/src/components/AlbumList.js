b40a374624483712c03a3a8807b392bbdd697781
Ramon Boza <bozanayade@gmail.com>
2018-07-26 17:12:16 +0200

    Functional component to class component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List!!!!!</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    render () {
+        return (
+            <View>
+                <Text>Album List!!!!!</Text>
+            </View>
+        );
+     }
+}
 
 export default AlbumList;
