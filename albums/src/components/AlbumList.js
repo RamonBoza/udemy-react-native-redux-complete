8ac7cadffa6c904f53b83ac624b7f5b969097e1c
Ramon Boza <bozanayade@gmail.com>
2018-07-26 17:56:57 +0200

    Refactored AlbumList to create AlbumDetails

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+    }
+
     render () {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List!!!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
      }
