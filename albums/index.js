e5ea9352ad510d99b65e00b0cfd784b6be042d8a
Ramon Boza <bozanayade@gmail.com>
2018-07-27 17:30:17 +0200

    Section 8 - making app scrollable and fit size

--- albums/index.js
+++ albums/index.js
@@ -7,7 +7,7 @@ import AlbumList from './src/components/AlbumList';
 // Create a component
 const App = () => {
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <Header headerText={'Albums'}/>
             <AlbumList/>
         </View>
