91e7a5aaebf9ff440167195076613e3580a3804a
Ramon Boza <bozanayade@gmail.com>
2018-07-27 15:54:55 +0200

    Added Card and CardSection as wrapper to stylish our Album - end of Section 7

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return(
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <Text>{props.album.title}</Text>
+            </CardSection>
+
+            <CardSection>
+                <Text>{props.album.title}</Text>
+            </CardSection>
+        </Card>
     );
 };
 
