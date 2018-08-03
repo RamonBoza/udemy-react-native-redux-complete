b4a34d1f791ee9749a53a1d44e4d4d73489bc05e
Ramon Boza <bozanayade@gmail.com>
2018-08-03 15:26:14 +0200

    Section 10 - Simple auth flow

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from "./common";
 
 class LoginForm extends Component {
 
     state = {email: '', password: ''};
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 
     render() {
         return (
@@ -28,7 +35,7 @@ class LoginForm extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress}>
                         Login
                     </Button>
                 </CardSection>
