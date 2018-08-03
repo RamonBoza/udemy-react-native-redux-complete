1985e7bba6cccef9af61f1a0d684b66e8a911ea4
Ramon Boza <bozanayade@gmail.com>
2018-08-03 17:39:27 +0200

    Section 10 - wrap up - adding full lifecycle for login/logout with firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common'
+import { Header, Button, Spinner } from './components/common'
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+    state = { loggedIn: null }
 
     componentWillMount() {
         firebase.initializeApp({
@@ -14,13 +16,33 @@ class App extends Component {
             storageBucket: 'auth-d7d20.appspot.com',
             messagingSenderId: '615650312257'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({loggedIn: true});
+            }
+            else {
+                this.setState({loggedIn: false});
+            }
+        });
+    }
+
+    renderContent() {
+        switch(this.state.loggedIn) {
+            case true:
+                return <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>;
+            case false:
+                return <LoginForm/>;
+            default:
+                return <Spinner size="large"/>;
+        }
     }
 
     render() {
         return (
             <View>
-                <Header headerText="Authentiation" />
-                <LoginForm />
+                <Header headerText="Authentication" />
+                {this.renderContent()}
             </View>
         );
     }
