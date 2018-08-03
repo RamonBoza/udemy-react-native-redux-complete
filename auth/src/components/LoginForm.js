ec72759ccbf3f7935bb64f7e768b8d4b1e8df04d
Ramon Boza <bozanayade@gmail.com>
2018-08-03 15:55:33 +0200

    Adding Login/Create user flow

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from "./common";
 
 class LoginForm extends Component {
 
-    state = {email: '', password: ''};
+    state = {email: '', password: '', error: ''};
 
     onButtonPress() {
         const { email, password } = this.state;
 
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed.'});
+                    });
+            });
     }
 
     render() {
@@ -34,13 +41,25 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button onPress={this.onButtonPress}>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
