5f182591c78fc45b9869d1b64babd38c236a42cd
Ramon Boza <bozanayade@gmail.com>
2018-08-03 16:05:12 +0200

    Section 10 - Adding spinner to our reusable components

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -10,6 +10,8 @@ class LoginForm extends Component {
     onButtonPress() {
         const { email, password } = this.state;
 
+        this.setState({ error: ''});
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
