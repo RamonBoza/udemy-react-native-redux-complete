536e9c27b5ea0be3d7173897f607d528ca512770
Ramon Boza <bozanayade@gmail.com>
2018-09-18 09:40:16 +0200

    Section 13 - Adding our libraries to the component

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
 
     renderItem(library) {
-
+        return <ListItem library={library} />
     }
 
     render() {
