b196f732cac40af0f4add70827f25139dbb41ab4
Ramon Boza <bozanayade@gmail.com>
2018-09-18 14:35:47 +0200

    Updating selection reducer and list item to show description

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from "./common";
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    renderDescription() {
+
+        const { library, selectedLibraryId } = this.props;
+
+        if(library.item.id === selectedLibraryId ) {
+            return (
+              <Text>{this.props.library.item.description}</Text>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library.item;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.item.title}
-                    </Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -23,4 +43,8 @@ const styles = {
     }
 };
 
-export default ListItem;
+const mapStateToProps = state => {
+    return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,3 +1,10 @@
-export default () => {
-    return null;
+export default (state = null, action) => {
+    console.log(action);
+    switch(action.type) {
+        case "select_library":
+            console.log(action.payload);
+            return action.payload;
+        default:
+            return state;
+    }
 };
