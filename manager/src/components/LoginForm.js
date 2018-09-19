6d5856d9758d7bd6f9a0d6f7022095f47bd85aef
Ramon Boza <bozanayade@gmail.com>
2018-09-19 10:22:19 +0200

    Adding auth reducer and basic boilerplate to it

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
+import { emailChanged } from '../actions';
 
 class LoginForm extends Component {
+
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
+
     render() {
         return(
             <Card>
@@ -9,6 +16,7 @@ class LoginForm extends Component {
                     <Input
                         label="Email"
                         placeholder="email@gmail.com"
+                        onChangeText={this.onEmailChange.bind(this)}
                     />
                 </CardSection>
                 <CardSection>
@@ -28,4 +36,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
