54ba0618795a27309a2c2bc1bed7ad8ae37771c8
Ramon Boza <bozanayade@gmail.com>
2018-09-20 16:25:03 +0200

    Section 16 - Finalizing the authentication flow with a Spinner

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
@@ -18,6 +18,18 @@ class LoginForm extends Component {
         const { email, password } = this.props;
 
         this.props.loginUser({ email , password });
+    }
+
+    renderButton() {
+        if(this.props.loading) {
+            return <Spinner size="large" />
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
     }
 
     renderError() {
@@ -56,9 +68,7 @@ class LoginForm extends Component {
                 {this.renderError()}
 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    { this.renderButton() }
                 </CardSection>
             </Card>
         );
@@ -77,7 +87,8 @@ const mapStateToProps = state => {
     return {
         email: state.auth.email,
         password: state.auth.password,
-        error: state.auth.error
+        error: state.auth.error,
+        loading: state.auth.loading
     };
 };
 
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -2,14 +2,16 @@ import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
     LOGIN_USER_SUCCESS,
-    LOGIN_USER_FAIL
+    LOGIN_USER_FAIL,
+    LOGIN_USER
 } from "../actions/types";
 
 const INITIAL_STATE = {
     email: '',
     password: '',
     user: null,
-    error: ''
+    error: '',
+    loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -21,9 +23,11 @@ export default (state = INITIAL_STATE, action) => {
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, error: '' };
+            return { ...state, ...INITIAL_STATE, user: action.payload};
         case LOGIN_USER_FAIL:
-            return { ...state, error: 'Authentication Failed.' };
+            return { ...state, error: 'Authentication Failed.', loading:false };
+        case LOGIN_USER:
+            return { ...state, loading: true, error: '', loading:false };
         default:
             return state;
     }
