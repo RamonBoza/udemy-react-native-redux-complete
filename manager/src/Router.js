4e8c89452bdfdb49eb79243714dbb20cd02f1433
Ramon Boza <bozanayade@gmail.com>
2018-09-28 09:13:33 +0200

    Section 17 - Adding another scene to the app

---
+++ manager/src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+
+const RouterComponent = () => {
+  return (
+      <Router>
+          <Scene key="root">
+              <Scene key="login" component={LoginForm} title="Please Login" />
+              <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          </Scene>
+      </Router>
+  );
+};
+
+export default RouterComponent;
