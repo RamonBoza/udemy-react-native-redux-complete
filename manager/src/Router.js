adc475b13d1f15b806b65f0ca3c2ef06dfe62304
Ramon Boza <bozanayade@gmail.com>
2018-10-03 18:26:51 +0200

    Adding employee create and form and some sugar from jsx

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
       <Router>
-          <Scene key="root">
-              <Scene key="login" component={LoginForm} title="Please Login" />
-              <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          <Scene key="root" hideNavBar>
+              <Scene key="auth">
+                <Scene key="login" component={LoginForm} title="Please Login" />
+              </Scene>
+              <Scene key="main" initial>
+                <Scene
+                    rightTitle="Add"
+                    onRight={() => { Actions.employeeCreate() }}
+                    key="employeeList"
+                    component={EmployeeList}
+                    title="Employees"
+                />
+                <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee"/>
+              </Scene>
           </Scene>
       </Router>
   );
