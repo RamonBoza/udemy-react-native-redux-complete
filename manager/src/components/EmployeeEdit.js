aaa7991e9088ab5c4c0646e98f47a9654a307abb
Ramon Boza <bozanayade@gmail.com>
2018-10-12 16:34:45 +0200

    Another refactor for edit and create employees

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,21 +1,29 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { Card, CardSection, Button } from "./common";
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from "../actions";
+import { employeeUpdate, employeeSave } from "../actions";
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
 
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({prop, value});
+        })
+
+    }
+
     onSaveButtonPress() {
         const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate( {name, phone, shift: shift || 'Monday' });
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
     onDeleteButtonPress() {
-        const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate( {name, phone, shift: shift || 'Monday' });
+
+
     }
 
 
@@ -24,7 +32,7 @@ class EmployeeEdit extends Component {
         return(
             <Card>
 
-                <EmployeeForm {...this.props} />
+                <EmployeeForm />
                 <CardSection>
                     <Button onPress={this.onSaveButtonPress.bind(this)}>
                         Save Changes
@@ -44,4 +52,4 @@ const mapStateToProps = (state) => {
 
     return { name, phone, shift };
 }
-export default connect(mapStateToProps, { employeeUpdate, employeeCreate } )(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave } )(EmployeeEdit);
